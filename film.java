a2796b0563de2b77367ac2ea11f4d4915bd258bd
Сорокин Алексей <jiysuan@mail.ru>
2022-12-17 21:04:16 +0300

    Переделал имена переменных

--- film.java
+++ film.java
@@ -3,17 +3,16 @@ public class film {
         String name;
         String genre;
         String director;
-        String lead_actor;
+        String leadActor;
         String country;
         int year;
 
-    
         public void info() {
             System.out.println();
             System.out.printf("Название фильма: %s \n", name);
             System.out.printf("Жанр: %s \n", genre);
             System.out.printf("Режиссёр: %s \n", director);
-            System.out.printf("В главных ролях: %s \n", lead_actor);
+            System.out.printf("В главных ролях: %s \n", leadActor);
             System.out.printf("Страна-производитель: %s \n", country);
             System.out.printf("Год выпуска: %d \n", year);
         }
--- programm.java
+++ programm.java
@@ -1,8 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 /*
 
 Написать приложение, которое хранит информацию о фильмах и позволяет получaть все детали о них.
@@ -10,67 +5,72 @@ import java.util.Scanner;
 
 */
 
-public class programm {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Programm {
 
     public static void main(String[] args) {
 
-        film iron_man = new film();
-        iron_man.name = "Iron Man";
-        iron_man.genre = "Супергеройский боевик, Фантастика, Фэнтези";
-        iron_man.director = "Джон Фавро";
-        iron_man.lead_actor = "Роберт Дауни Младший, Терренс Ховард, Джефф Бриджес, Гвинет Пэлтроу";
-        iron_man.country = "США";
-        iron_man.year = 2008;
-        
-        film iron_man_2 = new film();
-        iron_man_2.name = "Iron Man 2";
-        iron_man_2.genre = "Супергеройский боевик, Фантастика, Фэнтези";
-        iron_man_2.director = "Джон Фавро";
-        iron_man_2.lead_actor = "Роберт Дауни Младший, Дон Чидл, Скарлетт Йоханссон, Гвинет Пэлтроу, Микки Рурк";
-        iron_man_2.country = "США";
-        iron_man_2.year = 2010;
-        
-        film iron_man_3 = new film();
-        iron_man_3.name = "Iron Man 3";
-        iron_man_3.genre = "Супергеройский боевик, Фантастика, Фэнтези";
-        iron_man_3.director = "Шейн Блэк";
-        iron_man_3.lead_actor = "Роберт Дауни Младший, Дон Чидл, Гай Пирс, Гвинет Пэлтроу, Бен Кингсли";
-        iron_man_3.country = "США";
-        iron_man_3.year = 2013;
+        film ironMan = new film();
+        ironMan.name = "Iron Man";
+        ironMan.genre = "Супергеройский боевик, Фантастика, Фэнтези";
+        ironMan.director = "Джон Фавро";
+        ironMan.leadActor = "Роберт Дауни Младший, Терренс Ховард, Джефф Бриджес, Гвинет Пэлтроу";
+        ironMan.country = "США";
+        ironMan.year = 2008;
 
-        film incredible_hulk = new film();
-        incredible_hulk.name = "Incredible Hulk";
-        incredible_hulk.genre = "Супергеройский боевик, Фантастика, Фэнтези";
-        incredible_hulk.director = "Луи Летерье";
-        incredible_hulk.lead_actor = "Эдвард Нортон, Лив Тайлер, Тим Рот, Уильям Хёрт";
-        incredible_hulk.country = "США";
-        incredible_hulk.year = 2008;
+        film ironMan2 = new film();
+        ironMan2.name = "Iron Man 2";
+        ironMan2.genre = "Супергеройский боевик, Фантастика, Фэнтези";
+        ironMan2.director = "Джон Фавро";
+        ironMan2.leadActor = "Роберт Дауни Младший, Дон Чидл, Скарлетт Йоханссон, Гвинет Пэлтроу, Микки Рурк";
+        ironMan2.country = "США";
+        ironMan2.year = 2010;
+        
+        film ironMan3 = new film();
+        ironMan3.name = "Iron Man 3";
+        ironMan3.genre = "Супергеройский боевик, Фантастика, Фэнтези";
+        ironMan3.director = "Шейн Блэк";
+        ironMan3.leadActor = "Роберт Дауни Младший, Дон Чидл, Гай Пирс, Гвинет Пэлтроу, Бен Кингсли";
+        ironMan3.country = "США";
+        ironMan3.year = 2013;
+
+        film incredibleHulk = new film();
+        incredibleHulk.name = "Incredible Hulk";
+        incredibleHulk.genre = "Супергеройский боевик, Фантастика, Фэнтези";
+        incredibleHulk.director = "Луи Летерье";
+        incredibleHulk.leadActor = "Эдвард Нортон, Лив Тайлер, Тим Рот, Уильям Хёрт";
+        incredibleHulk.country = "США";
+        incredibleHulk.year = 2008;
 
         film tor = new film();
         tor.name = "Tor";
         tor.genre = "Супергеройский боевик, Фантастика, Фэнтези";
         tor.director = "Кеннет Брана";
-        tor.lead_actor = "Крис Хемсворт, Натали Портман, Том Хиддлстон, Стеллан Скарсгард, Колм Фиори, Рэй Стивенсон, Идрис Эльба, Кэт Деннингс, Рене Руссо, Энтони Хопкинс";
+        tor.leadActor = "Крис Хемсворт, Натали Портман, Том Хиддлстон, Стеллан Скарсгард, Колм Фиори, Рэй Стивенсон, Идрис Эльба, Кэт Деннингс, Рене Руссо, Энтони Хопкинс";
         tor.country = "США";
         tor.year = 2011;
 
-        film first_avenger = new film();
-        first_avenger.name = "First Avenger";
-        first_avenger.genre = "Супергеройский боевик, Фантастика, Фэнтези, Приключения";
-        first_avenger.director = "Джо Джонстон";
-        first_avenger.lead_actor = "Крис Эванс, Томми Ли Джонс, Хьюго Уивинг, Хейли Этвелл, Себастиан Стэн, Доминик Купер, Нил Макдонаф, Дерек Люк, Стэнли Туччи";
-        first_avenger.country = "США";
-        first_avenger.year = 2011;
+        film firstAvenger = new film();
+        firstAvenger.name = "First Avenger";
+        firstAvenger.genre = "Супергеройский боевик, Фантастика, Фэнтези, Приключения";
+        firstAvenger.director = "Джо Джонстон";
+        firstAvenger.leadActor = "Крис Эванс, Томми Ли Джонс, Хьюго Уивинг, Хейли Этвелл, Себастиан Стэн, Доминик Купер, Нил Макдонаф, Дерек Люк, Стэнли Туччи";
+        firstAvenger.country = "США";
+        firstAvenger.year = 2011;
 
         film avengers = new film();
         avengers.name = "Avengers";
         avengers.genre = "Супергеройский боевик, Фантастика, Фэнтези, Приключения";
         avengers.director = "Джосс Уидон";
-        avengers.lead_actor = "Роберт Дауни Младший, Крис Эванс, Марк Руффало, Крис Хемсворт, Скарлетт Йоханссон, Джереми Реннер, Том Хиддлстон, Кларк Грегг, Коби Смолдерс, Стеллан Скарсгард, Сэмюэл Л. Джексон";
+        avengers.leadActor = "Роберт Дауни Младший, Крис Эванс, Марк Руффало, Крис Хемсворт, Скарлетт Йоханссон, Джереми Реннер, Том Хиддлстон, Кларк Грегг, Коби Смолдерс, Стеллан Скарсгард, Сэмюэл Л. Джексон";
         avengers.country = "США";
         avengers.year = 2012;
 
-        List<film> cinema = new ArrayList<film>(Arrays.asList(iron_man, iron_man_2, iron_man_3, incredible_hulk, tor, first_avenger, avengers));
+        List<film> cinema = new ArrayList<film>(Arrays.asList(ironMan, ironMan2, ironMan3, tor, firstAvenger, avengers));
 
         film user = new film();
         Scanner in = new Scanner(System.in);
